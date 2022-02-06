9340b15b2e9b1cc212cbf9bc9451331a2b0ff033
Michael Murphy <michael.w.murphy@outlook.com>
2022-02-06 09:20:10 +0000

    Fixed issue where wrong location letters were not being highlighted correctly when there were more guesses of a letter than were in the actual answer.

--- src/components/Grid/WordField.tsx
+++ src/components/Grid/WordField.tsx
@@ -1,3 +1,5 @@
+import { match } from "assert";
+import { useState } from "react";
 import { LetterField, LetterState } from "./LetterField";
 
 interface WordFieldProps {
@@ -6,15 +8,21 @@ interface WordFieldProps {
   isActive: boolean;
 }
 
-export const WordField = ({ answer, guess, isActive }: WordFieldProps) => {
+export const WordField = ({
+  answer,
+  guess,
+  isActive: isActiveWord,
+}: WordFieldProps) => {
   if (!guess) {
     return (
       <div className="grid__word">
-        <LetterField letterState={LetterState.Unknown} letter={"\u00A0"} />
-        <LetterField letterState={LetterState.Unknown} letter={"\u00A0"} />
-        <LetterField letterState={LetterState.Unknown} letter={"\u00A0"} />
-        <LetterField letterState={LetterState.Unknown} letter={"\u00A0"} />
-        <LetterField letterState={LetterState.Unknown} letter={"\u00A0"} />
+        {Array.from(answer).map((_, idx) => (
+          <LetterField
+            key={idx}
+            letterState={LetterState.Unknown}
+            letter={"\u00A0"}
+          />
+        ))}
       </div>
     );
   }
@@ -24,24 +32,80 @@ export const WordField = ({ answer, guess, isActive }: WordFieldProps) => {
     Array(5 - guessLetters.length).fill("\u00A0")
   );
 
-  const getLetterState = (
-    index: number,
-    letter: string,
-    answer: string
-  ): LetterState => {
-    const correctLetter = answer[index];
+  /**
+   * Count the number of occurrences of letter in word
+   * @param word the word to be checked
+   * @param letter the letter to be counted in the word
+   * @returns the number of occurrences
+   */
+  const countLetterOccurrences = (word: string, letter: string): number => {
+    return (word.match(new RegExp(letter, "g")) || []).length;
+  };
 
-    if (isActive) {
+  /**
+   * Returns an array of indexes of the given letter
+   * @param word the word to be searched over
+   * @param letter the letter to be searched for
+   * @returns an array of indexes of letter in word
+   */
+  const getLetterIndexes = (word: string, letter: string): number[] => {
+    return Array.from(word.matchAll(new RegExp(letter, "g")))
+      .filter((match) => {
+        return typeof match.index === "number" ? true : false;
+      })
+      .map((match) => {
+        if (match.index === undefined) return -1;
+        return match.index;
+      })
+      .filter((index) => index >= 0);
+  };
+
+  const getLetterState = (letterIndex: number, letter: string): LetterState => {
+    const correctLetter = answer[letterIndex];
+
+    if (isActiveWord) {
+      // A non-submitted word does not have the coloured styling
       return LetterState.Unknown;
     }
 
     if (letter === correctLetter) {
       return LetterState.CorrectLocation;
-    } else if (answer.includes(letter)) {
-      return LetterState.CorrectWrongLocation;
-    } else {
+    }
+
+    if (!answer.includes(letter)) {
       return LetterState.Incorrect;
     }
+
+    //
+    // The letter must be included in the answer but in an incorrect location
+    //
+
+    const guessLetterOccurrences = countLetterOccurrences(guess, letter);
+    const answerLetterOccurrences = countLetterOccurrences(answer, letter);
+    const diff = guessLetterOccurrences - answerLetterOccurrences;
+
+    if (diff === 0) {
+      // There are the same number of guesses of the letter as answers
+      return LetterState.CorrectWrongLocation;
+    }
+
+    const answerLetterIndexes = getLetterIndexes(answer, letter);
+    const guessLetterInexes = getLetterIndexes(guess, letter).filter(
+      (letterIndex) => !answerLetterIndexes.includes(letterIndex)
+    );
+
+    // We only want to highlight an appropriate number of letters as wrong location so...
+    // Remove letter indexes where there are more than the number of correct letters in the andwer
+    // and determine if the index of the current letter is contained within the array
+    const highlightThisLetter = guessLetterInexes
+      .filter((_, idx) => idx < diff)
+      .includes(letterIndex);
+
+    if (highlightThisLetter) {
+      return LetterState.CorrectWrongLocation;
+    }
+
+    return LetterState.Incorrect;
   };
 
   return (
@@ -50,7 +114,7 @@ export const WordField = ({ answer, guess, isActive }: WordFieldProps) => {
         <LetterField
           key={idx}
           letter={letter}
-          letterState={getLetterState(idx, letter, answer)}
+          letterState={getLetterState(idx, letter)}
         />
       ))}
     </div>
